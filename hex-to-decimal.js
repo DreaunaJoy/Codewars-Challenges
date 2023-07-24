52eaf38358fa3b53317d950d538f95a70f92ab66
dreaunajoy <dreaunagibson@gmail.com>
2023-07-24 10:33:16 -0700

    added new codewars challenge

---
+++ hex-to-decimal.js
@@ -0,0 +1,12 @@
+//Complete the function which converts hex number (given as a string) to a decimal number.
+
+//solution 1
+
+function hexToDec(hexString){
+    return  parseInt(hexString, 16);
+   }
+
+
+//soltuion 2 
+
+const hexToDec = h => parseInt(h, 16);
