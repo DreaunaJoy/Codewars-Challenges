0484662bebd90bb11e2d55aad2918f737d8e405a
dreaunajoy <dreaunagibson@gmail.com>
2023-10-23 10:07:42 -0700

    added new codewars challenge

---
+++ Exclamation-marks-series-2-Remove-all-exclamation-marks-from-the-end-of-sentence.js
@@ -0,0 +1,24 @@
+/*  
+
+Remove all exclamation marks from the end of sentence.
+
+Examples
+"Hi!"     ---> "Hi"
+"Hi!!!"   ---> "Hi"
+"!Hi"     ---> "!Hi"
+"!Hi!"    ---> "!Hi"
+"Hi! Hi!" ---> "Hi! Hi"
+"Hi"      ---> "Hi"
+
+*/
+
+//solution
+
+function remove(s)
+{
+    while(s && s.slice(-1) == "!") 
+    { 
+        s = s.slice(0,-1) 
+    }
+    return s;
+}
