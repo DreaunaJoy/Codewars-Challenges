ee60f229a9e90d6514116dcf2c161430da738c7e
dreaunajoy <dreaunagibson@gmail.com>
2024-08-19 07:05:34 -0700

    added new codewars challenge

---
+++ Inspiring-Strings.js
@@ -0,0 +1,14 @@
+/* Given a string of space separated words, return the longest word.
+If there are multiple longest words, return the rightmost longest word.
+
+Examples
+"red white blue"  =>  "white"
+"red blue gold"   =>  "gold"
+
+*/
+
+// solution
+
+function longestWord(str){
+    return str.split(' ').sort((b, a) => b.length - a.length).pop();
+  }
