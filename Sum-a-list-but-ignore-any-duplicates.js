890075423a890ea23685e406e71ef9d02a6bd11f
dreaunajoy <dreaunagibson@gmail.com>
2024-08-27 07:48:09 -0700

    added new codewars challenge

---
+++ Sum-a-list-but-ignore-any-duplicates.js
@@ -0,0 +1,12 @@
+/* Please write a function that sums a list, but ignores any duplicated items in the list.
+
+For instance, for the list [3, 4, 3, 6] the function should return 10,
+and for the list [1, 10, 3, 10, 10] the function should return 4.
+
+ */
+
+// solution
+
+function sumNoDuplicates(numList) {
+    return numList.reduce((a,b)=>numList.indexOf(b)===numList.lastIndexOf(b) ? a+b : a,0);
+  }
