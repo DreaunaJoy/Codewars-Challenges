a743c1d8cbf9c98cb4523110857361453864f8c9
dreaunajoy <dreaunagibson@gmail.com>
2024-09-14 06:08:17 -0700

    added new codewars challenge

---
+++ L2-Triple-X.js
@@ -0,0 +1,16 @@
+/*  Given a string, return true if the first instance of "x" in the string is immediately followed by the string "xx".
+
+"abraxxxas" → true
+"xoxotrololololololoxxx" → false
+"softX kitty, warm kitty, xxxxx" → true
+"softx kitty, warm kitty, xxxxx" → false
+Note :
+
+capital X's do not count as an occurrence of "x".
+if there are no "x"'s then return false
+
+*/
+
+// solution
+
+const tripleX = (s) => /^[^x]*x{3}/.test(s)
