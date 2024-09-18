aa313c9a7549b644e706965b7d1eb0c7db3adc79
dreaunajoy <dreaunagibson@gmail.com>
2024-09-18 06:06:45 -0700

    added new codewars challenge

---
+++ Changing-letters.js
@@ -0,0 +1,16 @@
+/* When provided with a String, capitalize all vowels
+
+For example:
+
+Input : "Hello World!"
+
+Output : "HEllO WOrld!"
+
+Note: Y is not a vowel in this kata.
+
+ */
+
+// solution
+
+const vowels = ["a", "e", "i", "o", "u"];
+const swap = s => s.split('').map(x => vowels.includes(x) ? x.toUpperCase() : x).join('');
