e86f3c46b9c18e0d74013966edbf7b6071e82a13
dreaunajoy <dreaunagibson@gmail.com>
2024-10-04 07:01:24 -0700

    added new codewars challenge

---
+++ Exclamation-marks-series-Remove-all-exclamation-marks-from-the-end-of-words.js
@@ -0,0 +1,18 @@
+/* Task
+Remove all exclamation marks from the end of words. Words are separated by a single space. There are no exclamation marks within a word.
+
+Examples
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi Hi"
+remove("!!!Hi !!hi!!! !hi") === "!!!Hi !!hi !hi"
+
+*/
+
+// solution
+
+function remove(s){
+    return s.replace(/\b!+/g, '');  
+  }
