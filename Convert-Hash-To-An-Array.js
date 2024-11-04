5e7cd90a552027db5d34261b15864ef173aa34b5
dreaunajoy <dreaunagibson@gmail.com>
2024-11-04 08:50:14 -0800

    added new codewars challenge

---
+++ Convert-Hash-To-An-Array.js
@@ -0,0 +1,12 @@
+/* Convert a hash into an array. Nothing more, Nothing less.
+
+{name: 'Jeremy', age: 24, role: 'Software Engineer'}
+should be converted into
+
+[["age", 24], ["name", "Jeremy"], ["role", "Software Engineer"]]
+
+*/
+
+//solution
+
+const convertHashToArray = o => Object.entries(o).sort();
