f5f231c2e33537d3960044c9ef2e754a7ad96d28
dreaunajoy <dreaunagibson@gmail.com>
2025-03-26 19:09:20 -0700

    added new codewars challenge

---
+++ Words-to-sentence.js
@@ -0,0 +1,11 @@
+/* Write function which will create a string from a list of strings, separated by space.
+
+Example:
+
+["hello", "world"] -> "hello world"
+
+ */
+
+//solution
+
+const wordsToSentence = words => words.join(' ');
