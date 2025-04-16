9a3cf80270996e71e9f75d8912515e33903beb0c
dreaunajoy <dreaunagibson@gmail.com>
2025-04-15 19:02:21 -0700

    added new codewars challenge

---
+++ From-A-to-Z.js
@@ -0,0 +1,21 @@
+/* Given a string indicating a range of letters, return a string which includes all the letters in that range, including the last letter.
+Note that if the range is given in capital letters, return the string in capitals also!
+
+Examples
+"a-z" ➞ "abcdefghijklmnopqrstuvwxyz"
+"h-o" ➞ "hijklmno"
+"Q-Z" ➞ "QRSTUVWXYZ"
+"J-J" ➞ "J"
+Notes
+A hyphen will separate the two letters in the string.
+You don't need to worry about error handling in this kata (i.e. both letters will be the same case and the second letter will not be before the first alphabetically).
+
+*/
+
+//solution
+
+function gimmeTheLetters(sp) {
+    let s = ""
+    for(let i = sp[0].charCodeAt(); i <= sp[2].charCodeAt(); i++) s += String.fromCharCode(i)
+    return s
+  }
