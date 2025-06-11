25c25a6d18b3434e233cf61944910367ca8b2b9f
dreaunajoy <dreaunagibson@gmail.com>
2025-06-10 20:41:43 -0700

    added new codewars challenge

---
+++ For-the-sake-of-argument.js
@@ -0,0 +1,25 @@
+/* Write a function named numbers.
+
+function should return True if all parameters are of the Number type.
+
+The function should accept any number of parameters.
+
+Example usage:
+
+numbers(1, 4, 3, 2, 5); // true
+numbers(1, "a", 3); // false
+numbers(1, 3, NaN); // true
+
+*/
+
+//solution
+
+function numbers () {
+    var params = Array.prototype.slice.call(arguments)
+    
+    return params.every(isNumber);
+  }
+  
+  function isNumber (arg) {
+    return typeof arg === 'number';
+  }
