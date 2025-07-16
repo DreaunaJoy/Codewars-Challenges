63241d167e2ae8b4c03f33bbc60afd14e28ef44c
dreaunajoy <dreaunagibson@gmail.com>
2025-07-15 22:09:16 -0700

    added new codewars challenge

---
+++ Binary-Pyramid.js
@@ -0,0 +1,27 @@
+/* Given two numbers m and n, such that 0 ≤ m ≤ n :
+
+convert all numbers from m to n (inclusive) to binary
+sum them as if they were in base 10
+convert the result to binary
+return as a string
+Example
+1, 4  -->  1111010
+
+because:
+    1  // 1 in binary is 1
++  10  // 2 in binary is 10
++  11  // 3 in binary is 11
++ 100  // 4 in binary is 100
+-----
+  122  // 122 in binary is 1111010
+
+  */
+
+  //solution
+
+  function BinaryPyramid(m, n) {
+    let sm = 0;
+    for (let i = m; i <= n; i++)     
+       sm += parseInt(i.toString(2));  
+    return sm.toString(2);
+  }
