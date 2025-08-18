d463fbeeee73866655f34acfc7926aad11308dd5
dreaunajoy <dreaunagibson@gmail.com>
2025-08-18 13:32:05 -0700

    added new codewars challenge

---
+++ x-marks-the-spot.js
@@ -0,0 +1,49 @@
+/* Given a two dimensional array, return the co-ordinates of x.
+
+If x is not inside the array, or if x appears multiple times, return [].
+
+The co-ordinates should be zero indexed in row-major order.
+You should assume you will always get an array as input. The array will only contain 'x's and 'o's.
+
+Examples
+Input: []
+Return an empty array if input is an empty array => []
+
+Input: [
+  ['o', 'o'],
+  ['o', 'o']
+]
+Return an empty array if no x found => []
+
+Input: [
+  ['x', 'o'],
+  ['o', 'x']
+]
+Return an empty array if more than one x found => []
+
+Input: [
+  ['x', 'o'],
+  ['o', 'o']
+]
+Return [0,0] when x at top left => [0, 0]
+
+Input: [
+  ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+  ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+  ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+  ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+  ['o', 'o', 'o', 'o', 'o', 'o', 'x', 'o'],
+  ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o']
+]
+Return [4,6] for the example above => [4, 6]
+
+*/
+
+//solution
+
+const xMarksTheSpot = (input) => {
+    const xs = input.filter(e => e.filter(e => e.indexOf('x') !== -1).length);
+    if (xs.length !== 1 || (xs.length === 1  && xs[0].filter(e => e === 'x').length !== 1)) return []
+    
+    return input.map((e, idx) => e.map((x, idx2) => x === 'x' ? [idx, idx2] : '').filter(e => e !== '')).filter(e => e.length > 0)[0][0]
+  }
