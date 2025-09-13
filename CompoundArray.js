de4b6776167e294e03554b86f94e685a42c213f3
dreaunajoy <dreaunagibson@gmail.com>
2025-09-13 12:39:28 -0700

    added new codewars challenge

---
+++ CompoundArray.js
@@ -0,0 +1,17 @@
+/* You have to create a method "compoundArray" which should take as input two int arrays of different length and return one int array with numbers of both arrays shuffled one by one.
+
+Input - {1,2,3,4,5,6} and {9,8,7,6} 
+Output - {1,9,2,8,3,7,4,6,5,6}
+
+*/
+
+//solution
+
+function compoundArray(a, b) {
+    var c = []
+    while (a.length || b.length) { 
+      if (a.length) c.push(a.shift())
+      if (b.length) c.push(b.shift())
+    }
+    return c
+  }
