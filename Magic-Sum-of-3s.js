86cec1338f0359b1292093fd361c7b593bf266cf
dreaunajoy <dreaunagibson@gmail.com>
2025-09-21 21:14:30 -0700

    added new codewars challenge

---
+++ Magic-Sum-of-3s.js
@@ -0,0 +1,16 @@
+/* The magic sum of 3s is calculated on an array by summing up odd numbers which include the digit 3.
+
+Complete the function which accepts an array of integers and returns its magic sum of 3s.
+
+Example: [3, 12, 5, 8, 30, 13] results in 16 (3 + 13)
+
+If there is no such number in the array, 0 should be returned.
+
+ */
+
+//solution
+
+magicSum= (numbers) => numbers
+  .filter((a)=> a.toString().includes('3') && a & 1)
+  .reduce((sum, currentVal)=> sum + currentVal, 0)
+  
