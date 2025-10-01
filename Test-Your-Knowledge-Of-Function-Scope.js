20f500d1a2af59c1ba72e24a80b7f08fb1a33657
dreaunajoy <dreaunagibson@gmail.com>
2025-09-30 21:33:46 -0700

    added new codewars challenge

---
+++ Test-Your-Knowledge-Of-Function-Scope.js
@@ -0,0 +1,15 @@
+/* Write a function that adds from two invocations.
+
+All inputs will be integers.
+
+add(3)(4)  // 7
+add(12)(20) // 32
+*/
+
+//solution
+
+function add(x) {
+    return function(y) {
+      return x + y
+    }
+  }
