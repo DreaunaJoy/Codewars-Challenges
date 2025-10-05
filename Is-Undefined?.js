f5364c4b49e05fc7f899b9eaeb6b3f267cd77c60
dreaunajoy <dreaunagibson@gmail.com>
2025-10-04 21:27:33 -0700

    added new codewars challenge

---
+++ Is-Undefined?.js
@@ -0,0 +1,15 @@
+/* Is Undefined?
+Help Timmy remove undefined values from his array, you are modifing a callback function which he uses on each element on his array.
+Task
+Your task is to finish the isUndefined method which returns true if the value is undefined :)
+
+*/
+
+//solution
+
+function isUndefined(value) {
+    if(typeof value === 'undefined') {
+      return true;
+    }
+    return false;
+  }
