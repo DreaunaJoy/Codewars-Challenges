a50a7845743ef602e29e7015558bdc74ccd5b5a6
dreaunajoy <dreaunagibson@gmail.com>
2025-10-10 22:07:51 -0700

    added new codewars challenge

---
+++ Chicken-Sexing.js
@@ -0,0 +1,18 @@
+/* Bob is a chicken sexer. His job is to sort baby chicks into a Male(M) and Female(F) piles. When bob can't guess can throw his hands up and declare it with a '?'.
+
+Bob's bosses don't trust Bob's ability just yet, so they have paired him with an expert sexer. All of Bob's decisions will be checked against the expert's choices to generate a correctness score.
+
+Scoring Rules
+When they agree, he gets 1 point.
+When they disagree but one has said '?', he gets 0.5 points.
+When they disagree completely, he gets 0 points.
+
+*/
+
+//solution
+
+function correctness(bobsDecisions, expertDecisions) {
+    return bobsDecisions
+      .map((x,i) => x == expertDecisions[i] ? 1 : x == '?' ? 0.5 : expertDecisions[i] == '?' ? 0.5 : 0)
+      .reduce((a,b)=>a+b, 0);
+  }
