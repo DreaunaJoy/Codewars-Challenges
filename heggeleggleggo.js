909af459cb1659cb1ed2375428ca2ad52f610245
dreaunajoy <dreaunagibson@gmail.com>
2025-10-18 21:43:51 -0700

    added new codewars challenge

---
+++ heggeleggleggo.js
@@ -0,0 +1,16 @@
+/* Egg Talk.
+
+Insert an egg after each consonant. If there are no consonants, there will be no eggs. Argument will consist of a string with only alphabetic characters and possibly some spaces.
+
+Example
+hello => heggeleggleggo
+
+eggs => egegggeggsegg
+
+FUN KATA => FeggUNegg KeggATeggA
+
+ */
+
+//solution
+
+const heggeleggleggo = word => word.replace(/([^aeiou\s])/gi, "$1egg");
