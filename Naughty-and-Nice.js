63cb78ede5b3205abdd987cc406df1696d6662e5
dreaunajoy <dreaunagibson@gmail.com>
2025-10-23 20:56:04 -0700

    added new codewars challenge

---
+++ Naughty-and-Nice.js
@@ -0,0 +1,28 @@
+/* In this kata, you will write a function that receives an array of string and returns a string that is either 'naughty' or 'nice'. Strings that start with the letters b, f, or k are naughty. Strings that start with the letters g, s, or n are nice. Other strings are neither naughty nor nice.
+
+If there is an equal amount of bad and good actions, return 'naughty'
+
+Examples:
+
+bad_actions = ['broke someone\'s window', 'fought over a toaster', 'killed a bug']
+whatListAmIOn(bad_actions)
+#-> 'naughty'
+good_actions = ['got someone a new car', 'saved a man from drowning', 'never got into a fight']
+what_list_am_i_on(good_actions)
+#-> 'nice'
+actions = ['broke a vending machine', 'never got into a fight', 'tied someone\'s shoes']
+what_list_am_i_on(actions)
+#-> 'naughty'
+
+*/
+
+//solution
+
+const  whatListAmIOn = actions => {
+    let [naughty,nice] = [0,0]
+    actions.forEach(a=>{
+      if ( /[bfk]/.test(a[0]) ) naughty += 1 
+      if ( /[gsn]/.test(a[0]) ) nice += 1
+    })
+    return nice>naughty ? 'nice' : 'naughty'
+  }
