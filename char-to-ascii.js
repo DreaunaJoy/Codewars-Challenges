53a46358af35c3be83ed93847559748ae6f5f43e
dreaunajoy <dreaunagibson@gmail.com>
2025-11-04 20:51:26 -0800

    added new codewars challenge

---
+++ char-to-ascii.js
@@ -0,0 +1,8 @@
+// Take a string and return a hash with all the ascii values of the characters in the string. Returns nil (Ruby), None (Python) or null (JavaScript) if the string is empty. The key is the character, and the value is the ascii value of the character. Repeated characters are to be ignored and non-alphebetic characters as well.
+
+ //solution
+
+ const charToAscii = string => {
+    string = string.replace(/[^0-9a-z]/gi, '');
+    return string ? [...string].reduce((h, c) => (h[c] = c.charCodeAt(0), h), {}) : null;
+  }
