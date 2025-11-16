5da585de9ef99cff867a9b70791a93d7e9c1d55b
dreaunajoy <dreaunagibson@gmail.com>
2025-11-15 22:33:04 -0800

    added new codewars challenge

---
+++ Game-Hit-the-target.js
@@ -0,0 +1,41 @@
+/* given a matrix n x n (2-7), determine if the arrow is directed to the target (x).
+There will be only 1 arrow '>' and 1 target 'x'
+An empty spot will be denoted by a space " ", the target with a cross "x", and the scope ">"
+Examples:
+given matrix 4x4:
+[
+  [' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' '], --> return true
+  [' ', '>', ' ', 'x'],
+  [' ', ' ', ' ', ' ']
+] 
+given matrix 4x4:
+[
+  [' ', ' ', ' ', ' '],
+  [' ', '>', ' ', ' '], --> return false
+  [' ', ' ', ' ', 'x'],
+  [' ', ' ', ' ', ' ']
+] 
+given matrix 4x4:
+[
+  [' ', ' ', ' ', ' '],
+  [' ', 'x', '>', ' '], --> return false
+  [' ', '', ' ', ' '],
+  [' ', ' ', ' ', ' ']
+] 
+
+In this example, only a 4x4 matrix was used, the problem will have matrices of dimensions from 2 to 7
+Happy hacking as they say!
+
+ */
+
+//solution
+
+const solution = m => m.some(row => {
+    let arrowIndex = row.indexOf('>');
+    let targetIndex = row.lastIndexOf('x');
+      
+    return (arrowIndex > -1) && (targetIndex > -1)
+      ? arrowIndex < targetIndex
+      : false;   
+ });
