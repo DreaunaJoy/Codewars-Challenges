8d4a0a092c288d8757059e8e0c87d374a75c61ac
dreaunajoy <dreaunagibson@gmail.com>
2025-12-17 21:38:53 -0800

    added new codewars challenge

---
+++ Smallest-Product.js
@@ -0,0 +1,16 @@
+/* Given a non-empty array of non-empty integer arrays, multiply the contents of each nested array and return the smallest total.
+
+Example
+input = [
+  [1, 5],
+  [2],
+  [-1, -3]
+]
+
+result = 2
+
+*/
+
+//solution
+
+const smallestProduct = a => Math.min(...a.map(e => e.reduce((p, c) => p * c)))
